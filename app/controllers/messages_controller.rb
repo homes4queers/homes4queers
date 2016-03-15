e8daf662255f9846438f2583928ea70f88cfa545
sofanisba <stefanov.tamara@gmail.com>
2016-03-14 21:51:14 -0400

    404 is now rad

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -24,6 +24,7 @@ class MessagesController < ApplicationController
   def new
    @message = @conversation.messages.new
   end
+
   def notification_mailer
     if @message.user_id == @conversation.sender_id
       @user = User.find(@conversation.recipient_id)
@@ -36,8 +37,7 @@ class MessagesController < ApplicationController
    @message = @conversation.messages.new(message_params)
      if @message.save
        MessageMailer.message_notification(notification_mailer).deliver_later
-      # redirect_to conversation_messages_path(@conversation)
-      render nothing: true
+      redirect_to conversation_messages_path(@conversation)
     else
       redirect_back_or_to conversation_messages_path(@conversation), alert: "Say something to send something"
      end

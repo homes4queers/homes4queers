c20cef52180954e33a5b615d4d7f56543362001c
Your Name <sylviapereira@gmail.com>
2016-03-20 18:58:55 -0400

    Add type of user to search

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,36 +1,26 @@
 class TagsController < ApplicationController
   def index
-      @q = Tag.ransack(params[:q])
-      if params[:q]
-        @listings = @q.result.distinct.page(params[:page]).per(10)
-        @q.build_condition if @q.conditions.empty?
-        @q.sorts = 'created_at desc' if @q.sorts.empty?
-        # @listings = Listing.near(params[:q])
-      elsif params[:latitude] && params[:longitude]
-        @listings = Listing.near([params[:latitude], params[:longitude]])
-      else
-        @listings = Listing.order("created_at DESC").page(params[:page]).per(3)
-        #  @listings = Listing.order("created_at DESC").page(params[:page])
-      end
-    @tags = ActsAsTaggableOn::Tag.all
+    @tags = ActsAsTaggableOn::Tag.all.order('taggings_count desc')
   end
 
   def show
     @tag =  ActsAsTaggableOn::Tag.find(params[:id])
     @users = User.tagged_with(@tag.name)
+    @tags = ActsAsTaggableOn::Tag.all.order('taggings_count desc')
   end
 end
 
-def index
-  @q = Listing.ransack(params[:q])
-
-
-  def search
-    index
-    render :index
-  end
-  # respond_to do |format|
-  #   format.html
-  #   format.js
-  # end
-end
+# @q =  ActsAsTaggableOn::Tag.ransack(params[:q])
+#   if params[:q]
+#     @tags = @q.result.distinct.page(params[:page]).per(10)
+#     @q.build_condition if @q.conditions.empty?
+#     @q.sorts = 'created_at desc' if @q.sorts.empty?
+#   else
+#     @tags = ActsAsTaggableOn::Tag.all
+#   end
+#
+#   def search
+#     index
+#     render :index
+#   end
+# end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,41 +1,5 @@
 module ApplicationHelper
-  
-  # def search_form_for(record, options = {}, &proc)
-  #   if record.is_a? Ransack::Search
-  #     search = record
-  #     options[:url] ||= polymorphic_path(
-  #       search.klass, format: options.delete(:format)
-  #       )
-  #   elsif record.is_a?(Array) &&
-  #   (search = record.detect { |o| o.is_a?(Ransack::Search) })
-  #     options[:url] ||= polymorphic_path(
-  #       options_for(record), format: options.delete(:format)
-  #       )
-  #   else
-  #     raise ArgumentError,
-  #     'No Ransack::Search object was provided to search_form_for!'
-  #   end
-  #   options[:html] ||= {}
-  #   html_options = {
-  #     class:  html_option_for(options[:class], search),
-  #     id:     html_option_for(options[:id], search),
-  #     method: :get
-  #   }
-  #   options[:as] ||= Ransack.options[:search_key]
-  #   options[:html].reverse_merge!(html_options)
-  #
-  #   form_for(record, options, &proc)
-  # end
-  # #
-  # def sort_link(search_object, attribute, *args, &block)
-  #       search, routing_proxy = extract_search_and_routing_proxy(search_object)
-  #       unless Search === search
-  #         raise TypeError, 'First argument must be a Ransack::Search!'
-  #       end
-  #       args.unshift(capture(&block)) if block_given?
-  #       s = SortLink.new(search, attribute, args, params, &block)
-  #       link_to(s.name, url(routing_proxy, s.url_options), s.html_options(args))
-  #     end
+
 
   private
 

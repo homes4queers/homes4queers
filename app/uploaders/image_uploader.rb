122b88609b0710be7f00ceb6c52ea694df426d04
sofanisba <stefanov.tamara@gmail.com>
2016-03-22 13:10:25 -0400

    resize!

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -41,7 +41,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
-  # process resize_to_fit: [500,500]
+  process resize_to_fit: [500,500]
   #
   # def scale(width, height)
   #   # do something

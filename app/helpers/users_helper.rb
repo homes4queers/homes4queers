acb806629c2bf267e0a25b343ec844bc5c3635c7
sofanisba <stefanov.tamara@gmail.com>
2016-03-31 19:41:46 -0400

    refactor favourites on index

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,17 +1,20 @@
 module UsersHelper
 
   def favourite_button(user)
-    unless current_user.favourite_users.exists?(user.id)
-      link_to content_tag(:i, "favorite", class: "material-icons fave", alt: "favourite button", data: {disable_with: "blaaaah"}),
-      favourite_user_path(user.id),
-      method: :post,
-      remote: true
-    else
-      link_to content_tag(:i, "favorite", class: "material-icons unfave", alt: "unfavourite button"),
-      unfavourite_user_path(user.id),
-      method: :delete,
-      remote: true
-    end
+    # unless current_user.id == @user.id
+    user = user.id
+      unless current_user.favourite_users.exists?(user)
+        link_to content_tag(:i, "favorite", class: "material-icons fave", alt: "favourite button" ),
+        favourite_user_path(user),
+        method: :post,
+        remote: true
+      else
+        link_to content_tag(:i, "favorite", class: "material-icons unfave", alt: "unfavourite button"),
+        unfavourite_user_path(user),
+        method: :delete,
+        remote: true
+      end
+    # end
   end
 
 end

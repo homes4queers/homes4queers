86d35ce4e145d1b643b9e3b03ca68ba4f9405050
sofanisba <stefanov.tamara@gmail.com>
2016-04-04 11:54:07 -0400

    add polymorphic association to comments factory

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -2,6 +2,6 @@ FactoryGirl.define do
   factory :first_comment, class: 'Comments' do
     body "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
     association :user, factory: :test_user
-
+    association :commentable
   end
 end
